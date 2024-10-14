77dca493fdca2b8aea20f7db4f8a5cb35ff7484e
Fredrik Bergelin <fredrik.bergelin@gmail.com>
2024-10-14 20:30:17 +0200

    Increment version

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "Attack Radius Displays Firing Randomness"
 uid = "208fef88-1d0a-4d54-be68-f5d0d0961c5c"
-version = 2
+version = 3
 copyright = "WTFPL"
 description = [[Adds a decal based on distance and firing randomness to display overall spread of damage when the attack radius decal is shown.
 The estimate works best for high arc artillery. Low arc weapons generally have a smaller and less circular spread than shown.
